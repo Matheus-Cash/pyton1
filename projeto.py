5641a7a3b2cc70ca11192a370337ea17511f3e63
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-02-27 18:14:42 +0000

    214

--- projeto.py
+++ projeto.py
@@ -6,10 +6,13 @@
 
 enforcou = false
 
-while(not enforcou and not acertou)
-chute = input ('Qual a letra')
-chute = chute.strip()
-
+while(not enforcou and not acertou);
+chute = input ('Qual a letra');
+chute = chute.strip();
+index = 0 
 for letra input palavra_secreta:
-      if(chute == letra)
-      print (f'Encontrei a letra {letra} na posicao {index}')
+      if(chute.upper() == letra.upper());
+      print (f''Encontrei a letra {letra} na posicao {index}'');
+      index = index +
+      print('jogando');
+      print('fim de jogo');
