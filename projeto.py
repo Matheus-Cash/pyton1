baee9ef1984d527ddd175180042dfaab511918f9
Matheus-Cash <149600579+Matheus-Cash@users.noreply.github.com>
2025-03-20 14:16:57 -0300

    24

--- projeto.py
+++ projeto.py
@@ -1,18 +1,19 @@
+python
+enforcou = False
+acertou = False
+palavra_secreta = "exemplo"  # Defina a palavra secreta aqui
 
+while not enforcou and not acertou:
+    chute = input('Qual a letra? ')
+    chute = chute.strip()
+    index = 0
+    
+    for letra in palavra_secreta:
+        if chute.upper() == letra.upper():
+            print(f'Encontrei a letra {letra} na posição {index}')
+        index += 1  
+    
+    print('Jogando...')
+    
 
-
-
-
-
-enforcou = false
-
-while(not enforcou and not acertou);
-chute = input ('Qual a letra');
-chute = chute.strip();
-index = 0 
-for letra input palavra_secreta:
-      if(chute.upper() == letra.upper());
-      print (f''Encontrei a letra {letra} na posicao {index}'');
-      index = index +
-      print(''jogando...'')
-      print(''fim de jogo...'')
+print('Fim de jogo...')
